b12065a29c7c98105c99429341b4e45ae18a20b8
ruigomes99 <yruigomes99@gmail.com>
2024-08-31 14:25:02 +0100

    support new db design and improve logs

--- client/grpc.py
+++ client/grpc.py
@@ -5,7 +5,7 @@ import client.protos.ModelRunnerService_pb2_grpc as model_pb2_grpc
 from config import GRPC_MODEL_RUNNER_HOST
 
 
-def update_running_state(state, run_id, container_id=None, exit_code=None, logs=None):
+def update_running_state(run_id, state, result=None, logs=None):
     channel = grpc.insecure_channel(GRPC_MODEL_RUNNER_HOST)
     stub = model_pb2_grpc.ModelRunnerServiceStub(channel)
 
@@ -13,12 +13,13 @@ def update_running_state(state, run_id, container_id=None, exit_code=None, logs=
         run_id=run_id,
         state=state,
     )
-    if container_id is not None:
-        state_request.container_id = container_id
-    if exit_code is not None:
-        state_request.container_exit_code = exit_code
+    if result is not None:
+        state_request.result = str(result)
     if logs is not None:
         state_request.logs = str(logs)
 
     res = stub.UpdateRunninState(state_request)
-    print(f"gRPC Response: resolved={res.resolved}, error={res.err}")
+    print(
+        f"run_id={run_id}: Attempting to update state={state}\n"
+        f"grpc_response: resolved={res.resolved}, error={res.err}"
+    )
--- rabbitmq/processor.py
+++ rabbitmq/processor.py
@@ -15,8 +15,8 @@ def process_message(ch, method, properties, body):
         mem_limit = message.get("mem_limit", "256M")
         nano_cpus = message.get("cpu_percentage", 500000000)
 
-        grpc.update_running_state("building", run_id)
-        _, container = docker.build_and_run_container(
+        grpc.update_running_state(run_id, "building")
+        _, _ = docker.build_and_run_container(
             run_id,
             model_type,
             folder_path,
@@ -25,14 +25,14 @@ def process_message(ch, method, properties, body):
             mem_limit,
             nano_cpus,
         )
-        grpc.update_running_state("running", run_id, container.id)
+        grpc.update_running_state(run_id, "running")
 
     except Exception as e:
         grpc.update_running_state(
-            state="failed",
             run_id=run_id,
+            state="failed",
             logs=f"Container build/run failed: {e.args[0]}",
         )
-        print("Container build/run failed:", str(e))
+        print(f"run_id={run_id}: Container build/run failed:\n{str(e)}")
     finally:
         ch.basic_ack(delivery_tag=method.delivery_tag)
